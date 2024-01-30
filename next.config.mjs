050c3cc07ad83d5206ed49b07bf6c41d42e28752
Jun Du <junqdu@gmail.com>
2024-01-30 10:20:00 -0800

    add assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'docs',
+    assetPrefix: 'https://junqdu.github.io/ci-reader/',
+    // distDir: 'docs',
     // async redirects() {
     //     return [
     //         // Basic redirect
