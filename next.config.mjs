4c6547d5cebae78b36c57a5c7e40ad032e027a41
Jun Du <junqdu@gmail.com>
2024-01-30 09:57:19 -0800

    Update docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    // distDir: 'docs',
+    distDir: 'docs',
     // async redirects() {
     //     return [
     //         // Basic redirect
